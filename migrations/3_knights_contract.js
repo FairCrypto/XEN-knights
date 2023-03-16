5035d4424714568bbb28c204ac8353b61a805f7c
Leonid Belyaev <lbeliaev@gmail.com>
2023-03-15 17:53:04 -0700

    fixed taproot signature

--- migrations/3_knights_contract.js
+++ migrations/3_knights_contract.js
@@ -3,12 +3,12 @@ const XENKnights = artifacts.require("XENKnights");
 
 require("dotenv").config();
 
-const { XK_DELAY = 100, XK_DURATION = 1} = process.env;
+const { XK_DELAY = 10, XK_DURATION = 1} = process.env;
 
 module.exports = async function (deployer, network) {
 
-    const xenKnightsStartTs = process.env[`${network.toUpperCase()}_KNIGHTS_START_TS`] || Date.now();
-    const xenKnightsDuration = process.env[`${network.toUpperCase()}_KNIGHTS_DURATION`];
+    const xenKnightsStartTs = Number(process.env[`${network.toUpperCase()}_KNIGHTS_START_TS`]) || Date.now();
+    const xenKnightsDuration = Number(process.env[`${network.toUpperCase()}_KNIGHTS_DURATION`]) || XK_DURATION;
     const xenCryptoAddress = process.env[`${network.toUpperCase()}_CONTRACT_ADDRESS`];
     const delay = network === 'test' ? XK_DELAY : 0;
 
@@ -22,7 +22,7 @@ module.exports = async function (deployer, network) {
     await deployer.deploy(
         XENKnights,
         xenCrypto,
-        Math.floor(Number(xenKnightsStartTs) / 1000) + 10 + delay * 3600 * 24,
-        (Number(xenKnightsDuration) || 1)
+        Math.floor(xenKnightsStartTs / 1000) + 10 + delay * 3600 * 24,
+        (xenKnightsDuration || 1)
     );
 };
